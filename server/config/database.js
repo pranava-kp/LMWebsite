b3b765129ee552724a819ae944e83e71fd022b43
Pranava <pranavakp2004@gmail.com>
2026-02-25 20:05:02 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/staff_management', {
+    await mongoose.connect(process.env.MONGO_URL,{// || 'mongodb://localhost:27017/staff_management', {
       serverSelectionTimeoutMS: 5000 // Keep this for connection timeout
     });
     console.log('MongoDB connected successfully');
